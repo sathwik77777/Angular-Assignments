c5f4c835a9630b706790602e345678e054bcfbcf
sathwik77777 <93912523+sathwik77777@users.noreply.github.com>
2023-11-28 23:50:28 +0530

    Add files via upload

---
+++ app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { MovieComponent } from './movie/movie.component';
+
+export const routes: Routes = [
+    {path:"movie",component:MovieComponent}
+];
---
+++ movie.ts
@@ -0,0 +1,9 @@
+export class Movie {
+    constructor(
+    public movieId?: number,
+    public movieName?: string,
+    public lang?: string,
+    public releaseYear?: string,
+    public actor?: string,
+    public director?: string){}
+}
